615508cce273501c3fae45095d9abbac82263a4e
Anish Kesireddy <kesireddyanish@gmail.com>
2026-07-29 11:54:35 -0500

    Added ETL pipeline and connected to postgre database

---
+++ database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+DATABASE_URL = "postgresql://postgres:password@localhost:5433/sec_data"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+class InsiderTrade(Base):
+    __tablename__ = "insider_trades"
+
+    id = Column(Integer, primary_key=True, index=True)
+    company = Column(String, index=True)
+    share_owner = Column(String)
+    is_director = Column(String)
+    transaction_code = Column(String)
+    shares_traded = Column(Float)
+    price = Column(Float)
+    total_value = Column(Float)
+
+Base.metadata.create_all(bind=engine)
--- main.py
+++ main.py
@@ -2,35 +2,32 @@ from fastapi import FastAPI
 import requests
 import json
 from bs4 import BeautifulSoup
+from database import SessionLocal, InsiderTrade
+from apscheduler.schedulers.background import BackgroundScheduler
+import contextlib
 
-app = FastAPI(title="SEC Filings API")
-
-@app.get("/")
-def home():
-    return {"message": "Welcome to the SEC Filings API!"}
-
-
-@app.get("/api/apple")
-def get_apple_insider_data():
+def fetch_and_save_sec_data():
+    print("Fetching SEC data")
     headers = {
         "User-Agent": "Anish Kesireddy anish@example.com"
     }
     cik = "0000320193"
     url = f"https://data.sec.gov/submissions/CIK{cik}.json"
-    
+    #get the company data
     response = requests.get(url, headers=headers)
     company_data = response.json()
     recent_filings = company_data['filings']['recent']
     forms = recent_filings['form']
     accession_no = recent_filings['accessionNumber']
     primary_docs = recent_filings["primaryDocument"]
-    
+    #get the recent form 4 filings
     for i in range(len(forms)):
         if forms[i] == "4":
+            #remove the dashes from the accession number
             acc_no = accession_no[i].replace("-", "")
             xml_file = primary_docs[i].split("/")[-1]
             cik_no = str(int(cik))
-
+            #get the xml file
             xml_url = f"https://www.sec.gov/Archives/edgar/data/{cik_no}/{acc_no}/{xml_file}"
             xml_response = requests.get(xml_url, headers=headers)
             soup = BeautifulSoup(xml_response.content, 'xml')
@@ -48,13 +45,42 @@ def get_apple_insider_data():
             price = price_tag.text.strip() if price_tag and price_tag.text.strip() else "0.0"
             total_value = int(float(shares)) * float(price)
             
-            #return JSON format for api
-            return {
-                "company": "Apple",
-                "share_owner": share_owner,
-                "is_director": is_director,
-                "transaction_code": transaction_code,
-                "shares_traded": shares,
-                "price": price,
-                "total_value": round(total_value, 2)
-            }
+            db = SessionLocal()
+            new_trade = InsiderTrade(
+                company="Apple",
+                share_owner=share_owner,
+                is_director=is_director,
+                transaction_code=transaction_code,
+                shares_traded=float(shares),
+                price=float(price),
+                total_value=float(total_value)
+            )
+            
+            db.add(new_trade)
+            db.commit()
+            db.close()
+            print("Background Job: Data saved successfully!")
+            break
+#set up scheduler to run every hour
+@contextlib.asynccontextmanager
+async def lifespan(app: FastAPI):
+    scheduler = BackgroundScheduler()
+    fetch_and_save_sec_data() 
+    scheduler.add_job(fetch_and_save_sec_data, 'interval', hours=1) 
+    scheduler.start()
+    yield
+    scheduler.shutdown()
+
+app = FastAPI(title="SEC Filings API", lifespan=lifespan)
+
+#get the home page
+@app.get("/")
+def home():
+    return {"message": "Welcome to the SEC Filings API!"}
+#get all the trades from the database
+@app.get("/api/insider-trades")
+def get_insider_trades():
+    db = SessionLocal()
+    trades = db.query(InsiderTrade).all()
+    db.close()
+    return trades
